c9b20ac9566918452976af30d8766da091292b95
Humberto Gonzalez Sanchez <Humberto@MacBook-Pro-de-Humberto.local>
2020-04-20 11:30:58 -0500

    Created Project

---
+++ NQumicaInorganica/NQumicaInorganica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NQumicaInorganica
+//
+//  Created by Humberto Gonzalez Sanchez on 20/04/20.
+//  Copyright © 2020 Humberto Gonzalez Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
