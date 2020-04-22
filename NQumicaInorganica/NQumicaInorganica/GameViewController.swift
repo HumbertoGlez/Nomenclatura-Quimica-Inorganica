ab459b44dc6970464368c9f4b42e37882fde3580
Melba Consuelos <melbaconsuelos@hotmail.com>
2020-04-22 16:35:21 -0500

    Added Controllers for views and basic segues

---
+++ NQumicaInorganica/NQumicaInorganica/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  NQumicaInorganica
+//
+//  Created by Melba Consuelos on 22/04/20.
+//  Copyright © 2020 Humberto Gonzalez Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
